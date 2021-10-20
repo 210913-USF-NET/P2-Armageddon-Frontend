0977be36b08c9350172132bd4f2d6569a9546b7a
Brian Martinez <brian.martinez1356@gmail.com>
2021-10-20 18:09:52 -0400

    match list

--- Armageddon/src/app/app.module.ts
+++ Armageddon/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { MatchListComponent } from './match-list/match-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    MatchListComponent
   ],
   imports: [
     BrowserModule,
