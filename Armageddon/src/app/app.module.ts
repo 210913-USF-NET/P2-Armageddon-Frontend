82d5955132bdcb7e917d2f3e0ddd6a05206ce11d
Carlos A Martinez <Linguz2@gmail.com>
2021-10-21 21:27:19 -0400

    Auth0 initialization

--- Armageddon/src/app/app.module.ts
+++ Armageddon/src/app/app.module.ts
@@ -7,18 +7,25 @@ import { UserListComponent } from './user-list/user-list.component';
 import { MatchListComponent } from './match-list/match-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     MatchListComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'dev-9prkqivq.us.auth0.com',
+      clientId: 'THxpSzDld5aeNgJXovot3N6NJr3zq14u'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Armageddon/src/environments/environment.prod.ts
+++ Armageddon/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  authDomain: 'dev-9prkqivq.us.auth0.com',
+  authClientId: 'THxpSzDld5aeNgJXovot3N6NJr3zq14u'
 };
