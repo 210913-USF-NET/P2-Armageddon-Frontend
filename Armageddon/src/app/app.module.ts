cd2a930fac9064e6f68e55bb0dafe536f09dfe03
Carlos A Martinez <Linguz2@gmail.com>
2021-10-22 13:20:08 -0400

    Deleted and pasted the exact same line. :|

--- Armageddon/src/app/app.module.ts
+++ Armageddon/src/app/app.module.ts
@@ -13,15 +13,8 @@ import { LogoutButtonComponent } from './components/authentication components/lo
 import { AuthenticationButtonComponent } from './components/authentication components/authentication-button/authentication-button.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { PlayAsGuestComponent } from './components/authentication components/play-as-guest/play-as-guest.component';
-import { TitleCardComponent } from './components/title-card/title-card.component';
+import { TitleCardComponent } from './components/Title/title-card/title-card.component';
 import { LeaderboardComponent } from './components/Title/leaderboard/leaderboard.component';
-import { LayoutComponent } from './components/Lobby/layout/layout.component';
-import { LobbyComponent } from './components/Lobby/lobby/lobby.component';
-import { ShipsComponent } from './components/Lobby/ships/ships.component';
-import { ShipcustomizeComponent } from './components/Lobby/shipcustomize/shipcustomize.component';
-import { DefaultLayoutComponent } from './components/Lobby/default-layout/default-layout.component';
-import { ReadyComponent } from './components/Lobby/ready/ready.component';
-import { BoardComponent } from './components/Game/board/board.component';
 
 @NgModule({
   declarations: [
@@ -35,13 +28,6 @@ import { BoardComponent } from './components/Game/board/board.component';
     PlayAsGuestComponent,
     TitleCardComponent,
     LeaderboardComponent,
-    LayoutComponent,
-    LobbyComponent,
-    ShipsComponent,
-    ShipcustomizeComponent,
-    DefaultLayoutComponent,
-    ReadyComponent,
-    BoardComponent,
   ],
   imports: [
     BrowserModule,
