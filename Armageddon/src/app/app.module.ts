14ca6482182041d1a489198c35e90dd91901e573
Carlos A Martinez <Linguz2@gmail.com>
2021-10-22 12:18:09 -0400

    Merged?!!

--- Armageddon/src/app/app.module.ts
+++ Armageddon/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from '@auth0/auth0-angular';
-import { AuthComponent } from 'src/app/auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
