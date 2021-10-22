75162f0e00ed850b72a1dc7790e66f9fb6b00e50
Carlos A Martinez <Linguz2@gmail.com>
2021-10-22 13:14:58 -0400

    Made a bunch of components.

--- Armageddon/src/app/app.module.ts
+++ Armageddon/src/app/app.module.ts
@@ -14,6 +14,14 @@ import { AuthenticationButtonComponent } from './components/authentication compo
 import { ProfileComponent } from './pages/profile/profile.component';
 import { PlayAsGuestComponent } from './components/authentication components/play-as-guest/play-as-guest.component';
 import { TitleCardComponent } from './components/title-card/title-card.component';
+import { LeaderboardComponent } from './components/Title/leaderboard/leaderboard.component';
+import { LayoutComponent } from './components/Lobby/layout/layout.component';
+import { LobbyComponent } from './components/Lobby/lobby/lobby.component';
+import { ShipsComponent } from './components/Lobby/ships/ships.component';
+import { ShipcustomizeComponent } from './components/Lobby/shipcustomize/shipcustomize.component';
+import { DefaultLayoutComponent } from './components/Lobby/default-layout/default-layout.component';
+import { ReadyComponent } from './components/Lobby/ready/ready.component';
+import { BoardComponent } from './components/Game/board/board.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +34,14 @@ import { TitleCardComponent } from './components/title-card/title-card.component
     ProfileComponent,
     PlayAsGuestComponent,
     TitleCardComponent,
+    LeaderboardComponent,
+    LayoutComponent,
+    LobbyComponent,
+    ShipsComponent,
+    ShipcustomizeComponent,
+    DefaultLayoutComponent,
+    ReadyComponent,
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
