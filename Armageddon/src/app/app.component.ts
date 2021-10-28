a4b3d4672ee9277237846778db5150e691c18514
Woesel <tenz_woesel@yahoo.com>
2021-10-28 12:11:32 -0400

    Test

--- Armageddon/src/app/app.component.ts
+++ Armageddon/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Armaggedon";
+  title = "Armageddon";
 }
--- Armageddon/src/app/pages/home/home.component.spec.ts
+++ Armageddon/src/app/pages/home/home.component.spec.ts
@@ -1,25 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { HomeComponent } from './home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
+import { ArmageddonApiService } from 'src/app/service/armageddon-api.service';
+import { AuthModule, AuthService } from '@auth0/auth0-angular';
+import { ReplaySubject } from 'rxjs';
+import { ActivatedRouteStub } from 'src/app/testing/activatedRouteStub';
+
+
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
+    let activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
+    let service: ArmageddonApiService;
+    let router: Router;
+    let auth: AuthService;
+    let test: HomeComponent;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        // test = new HomeComponent(route, service, auth, router);
+        await TestBed.configureTestingModule({
+            declarations: [HomeComponent],
+            imports: [
+                RouterTestingModule, 
+                HttpClientTestingModule,  
+            ],
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: activatedRoute
+                }
+            ]
+        })
+            .compileComponents();
+        service = TestBed.inject(ArmageddonApiService);
+        router = TestBed.inject(Router);
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        
+        activatedRoute.setParams({id:5})
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
